73e4007f77faee9e0b07328ecac19b9623413933
Анна Агафонова <agan@sletat.ru>
2019-10-31 17:44:43 +0300

    Add project files.

---
+++ MyProject/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MyProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name: ");
+            string Name = Console.ReadLine();
+
+            Console.WriteLine("Enter your age: ");
+            int Age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter double: ");
+            double NumDouble = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter float: ");
+            float NumFloat = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("HI," + Name);
+            Console.WriteLine(Age);
+            Console.WriteLine(NumDouble);
+            Console.WriteLine(NumFloat);
+
+        }
+    }
+}
